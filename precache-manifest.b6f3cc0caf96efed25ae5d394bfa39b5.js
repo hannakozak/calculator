ed5965a6571e60c63878e63b1619b3cff0ac897e
Hanna Kozak <hannawkozak@gmail.com>
2019-10-22 22:43:19 +0100

    Updates

---
+++ precache-manifest.b6f3cc0caf96efed25ae5d394bfa39b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cba53421829da84ea87f5dc2f24318cb",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "a6d2fca76b03ae3ccd59",
+    "url": "/calculator/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "e3702fadcf29ac859c46",
+    "url": "/calculator/static/js/2.c837093c.chunk.js"
+  },
+  {
+    "revision": "a6d2fca76b03ae3ccd59",
+    "url": "/calculator/static/js/main.abab01af.chunk.js"
+  },
+  {
+    "revision": "81eb61f5a8d26d2fd52d",
+    "url": "/calculator/static/js/runtime-main.1d26180e.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/calculator/static/media/logo.25bf045c.svg"
+  }
+]);
