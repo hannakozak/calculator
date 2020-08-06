52965b0d8aa090ab3cc6c66dc17d7ea5c72a775f
Hanna Kozak <hannawkozak@gmail.com>
2020-08-06 23:07:31 +0100

    initial commit

---
+++ build/precache-manifest.c44b6f409d9d40ffeade1a7ec56a8066.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec487cc3dc7b4a753146ab43cf688bd7",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "1eb85c4e39bdc1bde6a2",
+    "url": "/calculator/static/css/main.5c131f9f.chunk.css"
+  },
+  {
+    "revision": "2e9e0c9b8a4046adc20d",
+    "url": "/calculator/static/js/2.7d222e3a.chunk.js"
+  },
+  {
+    "revision": "1eb85c4e39bdc1bde6a2",
+    "url": "/calculator/static/js/main.0f75981a.chunk.js"
+  },
+  {
+    "revision": "81eb61f5a8d26d2fd52d",
+    "url": "/calculator/static/js/runtime-main.1d26180e.js"
+  }
+]);
